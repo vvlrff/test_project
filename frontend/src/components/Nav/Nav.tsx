4d76e33731113dc86701f56c0fc0f0ae33fe335d
unhaten <csgold2002@mail.ru>
2023-10-09 21:53:59 +0300

    mnogo

---
+++ frontend/src/components/Nav/Nav.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import s from "./Nav.module.scss";
+
+const Nav = () => {
+    return (
+        <nav className={s.nav}>
+            <div className={s.left}>
+                <img
+                    src="https://thumb.tildacdn.com/tild3237-3232-4266-a362-333130353936/-/resize/104x/-/format/webp/_____2__1.png"
+                    alt=""
+                />
+            </div>
+            <div className={s.right}>
+                <NavLink className={s.link} to="/">
+                    Главная{" "}
+                </NavLink>
+                <NavLink className={s.link} to="/news">
+                    Новости{" "}
+                </NavLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
--- frontend/src/pages/NewsPage/NewsPage.tsx
+++ frontend/src/pages/NewsPage/NewsPage.tsx
@@ -1,69 +1,101 @@
 import { useState } from "react";
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Button } from '@mui/material';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { newsApi } from "../../services/newsApi";
 import NewsItem from "../../components/NewsItem/NewsItem";
-import s from "./NewsPage.module.scss"
+import s from "./NewsPage.module.scss";
+import { AiOutlineSearch } from "react-icons/ai";
+import Loader from "../../components/Loader/Loader";
 
 const NewsPage = () => {
     const [startDate, setStartDate] = useState<any>([]);
     const [endDate, setEndDate] = useState<any>([]);
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState("");
 
-    const { data: news, error, isLoading } = newsApi.useGetAllNewsQuery(message);
-    const [request, { data: requestData, isLoading: isRequestLoading, error: requestError }] = newsApi.usePostAllNewsMutation();
+    const {
+        data: news,
+        error,
+        isLoading,
+    } = newsApi.useGetAllNewsQuery(message);
+    const [
+        request,
+        { data: requestData, isLoading: isRequestLoading, error: requestError },
+    ] = newsApi.usePostAllNewsMutation();
 
     const sendData = async () => {
-        console.log({ "message": message, "start_date": startDate, "end_date": endDate })
-        console.log(request({ message: message, start_date: startDate, end_date: endDate }))
-        await request({ message: message, start_date: startDate, end_date: endDate });
-    }
+        console.log({
+            message: message,
+            start_date: startDate,
+            end_date: endDate,
+        });
+        console.log(
+            request({
+                message: message,
+                start_date: startDate,
+                end_date: endDate,
+            })
+        );
+        await request({
+            message: message,
+            start_date: startDate,
+            end_date: endDate,
+        });
+    };
 
     return (
-        <div>
-            <input
-                placeholder="Поиск новостей"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                type="text"
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                    label="От"
-                    value={startDate}
-                    onChange={(newValue) => setStartDate(newValue)}
-                />
-                <DatePicker
-                    label="До"
-                    value={endDate}
-                    onChange={(newValue) => setEndDate(newValue)}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={sendData}
-                >
-                    Искать
-                </Button>
-            </LocalizationProvider>
-
-            <div className={s.flex}>
-                <div className={s.container}>
-                    {isLoading && <h1>Идет загрузка</h1>}
-                    {error && <h1>Произошла ошибка</h1>}
-                    {news?.map(newsItem => <NewsItem key={newsItem.id} news={newsItem} />)}
+        <section>
+            <div className={s.search}>
+                <div className={s.searchLineContainer}>
+                    <input
+                        className={s.searchInput}
+                        placeholder="Поиск новостей"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        type="text"
+                    />
+                    <AiOutlineSearch />
                 </div>
-                <div className={s.container}>
-                    {isRequestLoading && <h1>Идет загрузка isRequestLoading</h1>}
-                    {requestError && <h1>Произошла ошибка requestError</h1>}
-                    {requestData?.map(requestDataItem =>
-                        <NewsItem key={requestDataItem.id} news={requestDataItem} />
-                    )}
+
+                <div className={s.dates}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="От"
+                            value={startDate}
+                            onChange={(newValue) => setStartDate(newValue)}
+                        />
+                        <DatePicker
+                            label="До"
+                            value={endDate}
+                            onChange={(newValue) => setEndDate(newValue)}
+                        />
+                        <button className={s.btn} onClick={sendData}>
+                            Искать
+                        </button>
+                    </LocalizationProvider>
                 </div>
             </div>
-        </div>
+
+            <div className={s.content}>
+                <div className={s.container}>
+                    {isLoading && <Loader></Loader>}
+                    {error && <h1>Произошла ошибка</h1>}
+                    {news?.map((newsItem) => (
+                        <NewsItem key={newsItem.id} news={newsItem} />
+                    ))}
+                </div>
+                <div className={s.container}>
+                    {isRequestLoading && <Loader></Loader>}
+                    {requestError && <h1>Произошла ошибка requestError</h1>}
+                    {requestData?.map((requestDataItem) => (
+                        <NewsItem
+                            key={requestDataItem.id}
+                            news={requestDataItem}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 };
 
