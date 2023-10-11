47309281018e7cf3d346d77cdea1e227dc7c7e31
Chief-tain <125179070+Chief-tain@users.noreply.github.com>
2023-10-11 14:23:45 +0300

    update cnannel list

--- TG_parser/db_postgres.py
+++ TG_parser/db_postgres.py
@@ -6,7 +6,7 @@ class PG_DB:
     def __init__(self) -> None:
         try:
             self.connection = psycopg2.connect(user="postgres",
-                                                password="4150",
+                                                password="0013jqfqA",
                                                 host="localhost",
                                                 port="5432",
                                                 dbname="AI_News")
