2ed0d03919c0cfb53731dd7beace41e9a1d9dc5f
unhaten <csgold2002@mail.ru>
2023-10-14 23:54:44 +0300

    improveMob

--- frontend/src/components/Messenger/Messenger.tsx
+++ frontend/src/components/Messenger/Messenger.tsx
@@ -10,9 +10,10 @@ const Messenger = () => {
     const [receivedMessages, setReceivedMessages] = useState<string[]>([]); // Состояние для полученных сообщений
     const [sentMessages, setSentMessages] = useState<string[]>([]); // Состояние для отправленных сообщений
     const [isMessengerOpen, setIsMessengerOpen] = useState<boolean>(false);
-    const [firstClick, setFirstClick] = useState<boolean>(false)
+    const [firstClick, setFirstClick] = useState<boolean>(false);
 
-    const [messageToBot, { data, isLoading, error }] = botApi.useSendToGptMessageMutation();
+    const [messageToBot, { data, isLoading, error }] =
+        botApi.useSendToGptMessageMutation();
 
     const handleOpen = (): void => {
         setIsMessengerOpen(true);
@@ -41,56 +42,54 @@ const Messenger = () => {
                     <motion.div
                         className={s.popup}
                         initial={{ opacity: 0, y: "100%", x: 75 }}
-                        animate={{ opacity: 1, y: '-10%' }}
+                        animate={{ opacity: 1, y: "-10%" }}
                         exit={{ opacity: 0, y: "100%" }}
                         transition={{ duration: 1 }}
                     >
-                        <div className={s.topContainer}>
-                            <div className={s.circle}></div>
-                        </div>
-                        <div className={s.line}></div>
-                        <div className={s.topContent}>
-                            <div
-                                className={s.iconContainer}
-                                onClick={handleClose}
-                            >
-                                <AiOutlineClose></AiOutlineClose>
+                        <div className={s.globalContainer}>
+                            <div className={s.topContainer}>
+                                <div className={s.circle}></div>
                             </div>
-                        </div>
-                        <div className={s.content}>
-                            {sentMessages.map((message, index) => (
-                                <div className={s.rightMessage} key={`sent-${index}`}>
-                                    {message}
+                            <div className={s.line}></div>
+                            <div className={s.topContent}>
+                                <div
+                                    className={s.iconContainer}
+                                    onClick={handleClose}
+                                >
+                                    <AiOutlineClose></AiOutlineClose>
                                 </div>
-                            ))}
-
-                            {receivedMessages.map((message, index) => (
-                                <div className={s.leftMessage} key={`received-${index}`}>
-                                    {message}
-                                </div>
-                            ))}
-
-                            {isLoading && <Loader></Loader>}
-                            {error && <h1>Произошла ошибка, попробуйте позже</h1>}
-
-                            <div className={s.flex}>
-                                <input
-                                    className={s.input}
-                                    value={message}
-                                    placeholder="Введите сообщение"
-                                    onChange={e => setMessage(e.target.value)}
-                                    type="text"
-                                />
-                                <button onClick={() => sendMessage()}>Отправить</button>
                             </div>
+                            <div className={s.content}>
+                                {sentMessages.map((message, index) => (
+                                    <div
+                                        className={s.rightMessage}
+                                        key={`sent-${index}`}
+                                    >
+                                        {message}
+                                    </div>
+                                ))}
 
-                            {/*                             
+                                {receivedMessages.map((message, index) => (
+                                    <div
+                                        className={s.leftMessage}
+                                        key={`received-${index}`}
+                                    >
+                                        {message}
+                                    </div>
+                                ))}
+
+                                {isLoading && <Loader></Loader>}
+                                {error && (
+                                    <h1>Произошла ошибка, попробуйте позже</h1>
+                                )}
+
+                                {/*                             
                             {sentMessages && <div className={s.rightMessage}>{sentMessages}</div>}
 
 */}
 
-                            {/* ЭТОТ КОД ЕСЛИ ОДНО СООБЩЕНИЕ БОТУ ГПТ */}
-                            {/* {data ? (
+                                {/* ЭТОТ КОД ЕСЛИ ОДНО СООБЩЕНИЕ БОТУ ГПТ */}
+                                {/* {data ? (
                                 <>
                                     <div className={s.leftMessage}>{data}</div>
                                     <div>Продолжить диалог, перейти на тг бота</div>
@@ -107,11 +106,34 @@ const Messenger = () => {
                                     <button onClick={sendMessage}>Отправить</button>
                                 </div>
                             )} */}
+                            </div>
+                            <div className={s.types}>
+                                <input
+                                    className={s.input}
+                                    value={message}
+                                    placeholder="Введите сообщение"
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    type="text"
+                                />
+                                <button
+                                    className={s.btn}
+                                    onClick={() => sendMessage()}
+                                >
+                                    Отправить
+                                </button>
+                            </div>
                         </div>
                     </motion.div>
                 )}
             </AnimatePresence>
-            <div className={firstClick ? `${s.messenger}` : `${s.messenger} ${s.firstTimeMessenger}`} onClick={handleOpen}></div>
+            <div
+                className={
+                    firstClick
+                        ? `${s.messenger}`
+                        : `${s.messenger} ${s.firstTimeMessenger}`
+                }
+                onClick={handleOpen}
+            ></div>
         </div>
     );
 };
