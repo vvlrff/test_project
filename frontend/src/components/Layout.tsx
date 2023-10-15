75bb7dde3e32cced4fbe8eb4f59e28bfa9b3b52e
unhaten <csgold2002@mail.ru>
2023-10-16 00:52:05 +0300

    newsNLoaderForMsngr

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,31 +1,32 @@
-import { Outlet } from 'react-router-dom';
-import Nav from './Nav/Nav';
-import Footer from './Footer/Footer';
-import Messenger from './Messenger/Messenger';
-import ProgressBar from './ProgressBar/ProgressBar';
-import { motion } from 'framer-motion'
-import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { selectAuth } from '../features/authSlice';
+import { Outlet } from "react-router-dom";
+import Nav from "./Nav/Nav";
+import Footer from "./Footer/Footer";
+import Messenger from "./Messenger/Messenger";
+import ProgressBar from "./ProgressBar/ProgressBar";
+import { motion } from "framer-motion";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { selectAuth } from "../features/authSlice";
 
 const Layout = () => {
-  const { access_token } = useAppSelector(selectAuth);
+    const { access_token } = useAppSelector(selectAuth);
 
-  return (
-    <>
-      <header>
-        <Nav />
-      </header>
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}>
-        <ProgressBar />
-        {access_token && <Messenger />}
-        <Outlet />
-      </motion.main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <header>
+                <Nav />
+            </header>
+            <motion.main
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+            >
+                <ProgressBar />
+                {access_token && <Messenger />}
+                <Outlet />
+            </motion.main>
+            <Footer />
+        </>
+    );
 };
 
 export default Layout;
