4ae4b260cc7dbc1914c35628f2ecf7a4da63d87c
unhaten <csgold2002@mail.ru>
2023-10-16 22:24:49 +0300

    awddawawd

--- frontend/src/components/NewsItem/NewsItem.tsx
+++ frontend/src/components/NewsItem/NewsItem.tsx
@@ -11,8 +11,8 @@ interface NewsItemProps {
 }
 
 const NewsItem: FC<NewsItemProps> = ({ news, animationVariants }) => {
-    const splitedMessage = news.msg.split("");
-    const slicedMessage = splitedMessage.slice(0, 100);
+    const splitedMessage = news.msg.split(" ");
+    const slicedMessage = splitedMessage.slice(0, 25);
     const cutMessage = slicedMessage.concat("...");
     const finalMessage = cutMessage.join(" ");
 
